83f632df376668b7637664c6af7490d42e9dc68f
Selta7 <132694122+Selta7@users.noreply.github.com>
2023-12-15 19:51:57 +0000

    After using the diagram this is what i have

--- src/models.py
+++ src/models.py
@@ -7,24 +7,40 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'User'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    user_name = Column(String(250), nullable=False)
+    first_name = Column(String(250), nullable=False)
+    last_name = Column(String(250), nullable=False)
+    age = Column(Integer, nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
+class Favorites(Base):
+    __tablename__ = 'Favorite'
+    fav_Id = Column (Integer, primary_key=True)
+    char_Id = Column (Integer, ForeignKey('Character.char_Id'),nullable=True)
+    planet_Id = Column (Integer, ForeignKey('Planet.planet_id'), nullable=True)
+    user_Id = Column (Integer, ForeignKey('User.id'))
+
+class Characters (Base):
+    __tablename__ = 'Character'
+    char_Id = Column (Integer, primary_key=True)
+    char_name = Column (String (250), nullable=False)
+    char_age = Column(Integer, nullable=False)
+    char_height = Column(Integer, nullable=False)
+
+
+class Planets(Base):
+    __tablename__ = 'Planet'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
-
+    planet_id = Column(Integer, primary_key=True)
+    planet_name = Column(String(250))
+    planet_mass = Column(String(250))
+    planet_environment = Column(String(250), nullable=False)
+    
     def to_dict(self):
         return {}
 
